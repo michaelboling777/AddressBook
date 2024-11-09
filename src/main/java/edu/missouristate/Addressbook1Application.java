46ffe53f67a42d6f9d64f1b2a9ee86934f58a151
michaelboling777 <michaelboling30@gmail.com>
2024-11-09 01:52:12 -0600

    spring boot first commit

---
+++ src/main/java/edu/missouristate/Addressbook1Application.java
@@ -0,0 +1,13 @@
+package edu.missouristate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Addressbook1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Addressbook1Application.class, args);
+    }
+
+}
